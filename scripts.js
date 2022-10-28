31526f71bd6f2c974192fbde41f27d0dec650f2f
Bordeaux2294 <ford.trishanna876@gmail.com>
2022-10-28 15:20:34 -0500

    Fixed newsletter and added button functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-/* Add your JavaScript to this file */
+window.onload = function(){
+    var email= document.getElementById('email');
+    const newsletter= document.getElementsByClassName('newsletter')[0];
+    const message= newsletter.getElementsByClassName('message')[0];
+    const button= newsletter.getElementsByTagName('button')[0];
+    
+    button.onclick = event =>{
+        event.preventDefault();
+        message.textContent  = (email.value == "" || !email.value.match( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) 
+        ? `Please enter a valid email address` :  `Thank you! Your email address ${email.value} has been added to our mailing list!`
+    } 
+}
